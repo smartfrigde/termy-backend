a7423dcc37e5b6d65adb9674332bf8141ea11dd1
PiotrSzwedo <piotrszwedo2007@gmail.com>
2025-05-16 17:28:33 +0200

    Added: System used to increment sync version

---
+++ laravel/app/Services/SynchronizationService.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\synchronizationVersions;
+use App\Models\Teams;
+
+class SynchronizationService
+{
+    public function incrementSynchVersion(array $usersIds){
+        synchronizationVersions::whereIn("user_id", $usersIds)->increment('version');
+    }
+
+    public function decrementSynchVersion(array $usersIds){
+        synchronizationVersions::whereIn('user_id', $usersIds)->decrement('version');
+    }
+
+    public function getUserIdsFromTeam(Teams $team){
+        $usersIds = [];
+
+        foreach ($team->user as $user){
+            $usersIds[] = $user->id;
+        }
+
+        return $usersIds;
+    }
+}
